dd5b544906288004c9180a8029a13ddfb0d3e2ce
vaishvik12 <www.vaishvikjamalta@gmail.com>
2026-05-08 15:22:30 +0530

    added css to header and home page. And added Images.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import {Login} from './assets/Login/Login'
+import {Header} from './Header.jsx'
+import {HomePage} from './HomePage.jsx'
 
 function App(){
 return (
   <div className = "app-container">
-
+    <Header/>
+    <HomePage/>
   </div>
 )
 }
